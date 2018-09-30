d3a63d580f95e9ab3e1e754a4d951e90949c4102
steemrss@https://github.com <Torrey@TorreyUmland.com>
2018-09-29 19:04:43 -0700

    Use blog as default user feed

--- src/routes/user.js
+++ src/routes/user.js
@@ -8,4 +8,9 @@ router.get(':username/:type', async (ctx, next) => {
     ctx.body = await rssGeneratorUser(ctx.params.username, ctx.params.type)   
 })
 
+router.get(':username', async (ctx, next) => {
+    ctx.type = 'text/xml'
+    ctx.body = await rssGeneratorUser(ctx.params.username, 'blog')   
+})
+
 export default router
